a904eacec77352ba39a77511f2cedfb3cd123de3
koraniar <eganfs@gmail.com>
2017-06-03 23:16:08 -0500

    android aplication base

--- app/src/main/java/com/koraniar/freebitcoin/MainActivity.java
+++ app/src/main/java/com/koraniar/freebitcoin/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onC reate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
